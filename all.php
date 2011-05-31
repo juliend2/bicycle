5c46cf1683c5245a4e00ae83439ea36b47a9a43d
Julien Desrosiers <juliend2@gmail.com>
2011-05-31 08:25:21 -0400

    load dependencies from all.php

--- all.php
+++ all.php
@@ -1,5 +1,11 @@
 <?php
 
+// deps:
+include_once 'lib/ez_sql/shared/ez_sql_core.php';
+include_once 'lib/ez_sql/mysql/ez_sql_mysql.php';
+include_once 'lib/julienphpformvalidation/validator.php';
+
+// core:
 include_once 'model.php';
 include_once 'functions.php';
 include_once 'helpers.php';
