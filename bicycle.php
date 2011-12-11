0d874c9a8faffc9424115ab89d3fc4a61b606a53
Julien Desrosiers <juliend2@gmail.com>
2011-12-10 21:41:25 -0500

    removed some dependencies

--- bicycle.php
+++ bicycle.php
@@ -3,7 +3,6 @@
 // dependencies:
 include_once 'ez_sql/shared/ez_sql_core.php';
 include_once 'ez_sql/mysql/ez_sql_mysql.php';
-include_once 'julienphpformvalidation/validator.php';
 
 // bicycle's files:
 include_once 'lib/dispatcher.php';
@@ -12,4 +11,3 @@ include_once 'lib/form.php';
 include_once 'lib/migrator.php';
 include_once 'lib/functions.php';
 include_once 'lib/helpers.php';
-include_once 'lib/crud.php';
